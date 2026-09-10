b7fd994f7c8ad71e4eacd98134179b7de92259df
MERYEM FARES <ashrshahom5@gmail.com>
2026-09-10 11:55:15 +0100

    Load SAFRA visual polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { StoreProvider } from './context/StoreContext';
 import './styles.css';
+import './polish.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
