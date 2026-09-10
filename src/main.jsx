46eddebe5df8e828bafaed23a0084c0a282d739a
MERYEM FARES <ashrshahom5@gmail.com>
2026-09-10 12:44:41 +0100

    Load final commerce experience styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { StoreProvider } from './context/StoreContext';
 import './styles.css';
 import './polish.css';
 import './mobile-ux.css';
+import './experience.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
