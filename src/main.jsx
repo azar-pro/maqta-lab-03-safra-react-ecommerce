50daa54de43cde97bb727d24be448648638a1345
MERYEM FARES <ashrshahom5@gmail.com>
2026-09-10 12:45:26 +0100

    Wrap SAFRA app with error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
+import ErrorBoundary from './components/ErrorBoundary';
 import { StoreProvider } from './context/StoreContext';
 import './styles.css';
 import './polish.css';
@@ -10,10 +11,12 @@ import './experience.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <StoreProvider>
-        <App />
-      </StoreProvider>
-    </HashRouter>
+    <ErrorBoundary>
+      <HashRouter>
+        <StoreProvider>
+          <App />
+        </StoreProvider>
+      </HashRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
