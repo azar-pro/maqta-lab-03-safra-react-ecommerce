fa0acd00cbd13eb2c8eb19e76775ad908d5b90f9
MERYEM FARES <ashrshahom5@gmail.com>
2026-09-13 20:56:51 +0100

    Load final V3 photography overrides

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ import './final-v5.css';
 import './final-v6.css';
 import './photo-v7.css';
 import './a11y-v8.css';
+import './photo-v9.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
